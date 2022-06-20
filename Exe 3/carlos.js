1aed08452c7254c1ef0905539cdbe0b6c61855ee
gabrielpascoli <99223332+gabrielpascoli@users.noreply.github.com>
2022-06-20 11:40:28 -0300

    Delete Exe 3 directory

--- Exe 3/carlos.js
+++
@@ -1,7 +0,0 @@
-function Ord(nomes){
-    let ns = nomes.value;
-    let arrNs = ns.split(", ");
-    let sort = arrNs.sort();
-    console.log(sort);
-    document.getElementById("resultado").innerHTML = sort;
-}
